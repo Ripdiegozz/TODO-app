07a25ed87c6aec7e5ad751906d45cf9bd6733b9b
Diego Garcia <diegogarciag63@gmail.com>
2023-01-05 15:49:44 -0500

    Max lenght enforced by js

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ import {addTask} from "./components/addTask.js"
 import { displayTask } from "./components/readTask.js";
 
 const btn = document.querySelector('[data-form-btn]');
+const input = document.querySelector('[data-form-input]');
+
 
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', addTask);
+
+let max = 35;
+
+input.addEventListener('keyup', function (event) {
+    event.target.value = event.target.value.substring(0, max)
+})
+
+
 displayTask();
